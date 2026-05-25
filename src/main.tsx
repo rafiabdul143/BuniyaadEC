974c46d423bc56a68ca535d5419966eb80735f36
Mohammed Rafi (Quadrant Technologies LLC) <mohdabdulrafi17@gmail.com>
2026-05-25 12:20:46 +0530

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import App from './App.tsx';
-import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-<HelmetProvider>
-  <App />
-</HelmetProvider>
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
 );
